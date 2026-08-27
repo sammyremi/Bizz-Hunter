b11d7991c6f91ab8becddd8886d350b5524b8163
sammyremi <adebayorsamuelas@gmail.com>
2026-08-27 22:41:42 +0100

    feat: custom route for search business

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  namespace :api do
+    namespace :v1 do
+      get "businesses/search", to: "business_discovery#search"
+    end
+  end
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
